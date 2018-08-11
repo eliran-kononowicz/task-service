63fae600e1a32d133743e0c7cbc09ec0516df357
Eliran Kononowicz <eliran.kononowicz@zeek.me>
2018-08-11 21:46:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const Koa = require("koa");
+const logger = require("koa-logger");
+const router = require("koa-router")();
+
+const app = new Koa();
+app.use(logger());
+const okResponse = ctx => {
+  ctx.body = "ok";
+};
+router.get("/liveness", okResponse).get("/readiness", okResponse);
+app.use(router.routes());
+app.listen(process.env.PORT);
